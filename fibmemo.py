f0517af2ca864f6d627ed7da2275533c49c49f11
Anjanna <anjanna.ramesh@gmail.com>
2021-06-20 12:25:07 +0530

    fib memoization

---
+++ fibmemo.py
@@ -0,0 +1,15 @@
+def fib(n, memo={}):
+    
+    if n in memo:
+        return memo[n]
+    
+    if n == 1 or n == 2:
+        return 1
+    
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+
+    return memo[n]
+
+print(fib(4))
+print(fib(5))
+print(fib(50))
